d100df27c898bae9f98288c483d09f17dc657b79
jorgeizaguirree <jorge@izaguirre.eu>
2024-12-10 20:01:03 +0100

    Solucion de app not found

--- app/src/main/java/upm/es/proyecto_app/LogInScreen.java
+++ app/src/main/java/upm/es/proyecto_app/LogInScreen.java
@@ -16,7 +16,6 @@ import androidx.core.view.WindowInsetsCompat;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
