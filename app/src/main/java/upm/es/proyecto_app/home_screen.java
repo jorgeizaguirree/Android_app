4d5f34307227517c890d762704eaec3a010a9d12
jorgeizaguirree <jorge@izaguirre.eu>
2024-12-10 18:11:55 +0100

    prueba url

--- app/src/main/java/upm/es/proyecto_app/home_screen.java
+++ app/src/main/java/upm/es/proyecto_app/home_screen.java
@@ -2,14 +2,35 @@ package upm.es.proyecto_app;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class home_screen extends AppCompatActivity {
+
+    public BufferedReader get_url(String url_string){
+        try {
+            URL url = new URL(url_string);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            Toast.makeText(home_screen.this, conn.getResponseCode() + "", Toast.LENGTH_SHORT).show();
+            Toast.makeText(home_screen.this, conn.getResponseMessage(), Toast.LENGTH_SHORT).show();
+            InputStream is = conn.getInputStream();
+            return new BufferedReader(new InputStreamReader(is));
+        }
+        catch (Exception e) {
+            Toast.makeText(home_screen.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        return null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +42,10 @@ public class home_screen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+
         TextView text = findViewById(R.id.homeScreen_txt_welcome);
         String welcome = "Welcome " + getIntent().getStringExtra("user");
         text.setText(welcome);
