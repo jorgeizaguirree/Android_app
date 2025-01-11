2bfc9c59c4e73bc0ac9553a1a00484296a325abd
tomasbeboshvili <tomas.beboshvili@alumnos.upm.es>
2025-01-11 20:51:48 +0100

    hecho el merge, falta cambiear homscreen actualizacion tasks, homescreen no va por que hay que ajustar el boton

--- app/src/main/java/upm/es/proyecto_app/home_screen.java
+++ app/src/main/java/upm/es/proyecto_app/home_screen.java
@@ -1,70 +1,70 @@
-package upm.es.proyecto_app;
+    package upm.es.proyecto_app;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+    import android.content.Intent;
+    import android.graphics.Bitmap;
+    import android.graphics.BitmapFactory;
+    import android.os.Bundle;
+    import android.widget.Button;
+    import android.widget.ImageView;
+    import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+    import androidx.activity.EdgeToEdge;
+    import androidx.appcompat.app.AppCompatActivity;
+    import androidx.core.graphics.Insets;
+    import androidx.core.view.ViewCompat;
+    import androidx.core.view.WindowInsetsCompat;
 
-import java.io.File;
-import java.util.List;
-import java.util.Random;
+    import java.io.File;
+    import java.util.List;
+    import java.util.Random;
 
-public class home_screen extends AppCompatActivity {
+    public class home_screen extends AppCompatActivity {
 
-    int quoteCounter;
-    List<Quotes> quotes;
-    Button url_btn;
-    TextView quote, welcome, description;
-    ImageView userImageView, settingsImageView;
+        int quoteCounter;
+        List<Quotes> quotes;
+        Button url_btn;
+        TextView quote, welcome, description;
+        ImageView userImageView, settingsImageView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_screen);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_home_screen);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
 
-        Random random = new Random();
-        quoteCounter = random.nextInt(10);
-        url_btn = findViewById(R.id.homeScreen_btn_createQuote);
-        quote = findViewById(R.id.homeScreen_txt_quote);
-        description = findViewById(R.id.homeScreen_txt_welcomeDescription);
-        welcome = findViewById(R.id.homeScreen_txt_username);
-        userImageView = findViewById(R.id.homeScreen_image_defaultUserIcon);
+            Random random = new Random();
+            quoteCounter = random.nextInt(10);
+            url_btn = findViewById(R.id.homeScreen_btn_createQuote);
+            quote = findViewById(R.id.homeScreen_txt_quote);
+            description = findViewById(R.id.homeScreen_txt_welcomeDescription);
+            welcome = findViewById(R.id.homeScreen_txt_username);
+            userImageView = findViewById(R.id.homeScreen_image_defaultUserIcon);
 
-        settingsImageView = findViewById(R.id.homeScreen_image_settings);
-        settingsImageView.setOnClickListener(v -> {
-            Intent intent = new Intent(home_screen.this, settingsScreen.class);
-            startActivity(intent);
-        });
+            settingsImageView = findViewById(R.id.homeScreen_image_settings);
+            settingsImageView.setOnClickListener(v -> {
+                Intent intent = new Intent(home_screen.this, settingsScreen.class);
+                startActivity(intent);
+            });
 
-        APIconnection api = new APIconnection(this, quotes, quote);
-        api.start();
-        quote.setText("Loading...");
+            APIconnection api = new APIconnection(this, quotes, quote);
+            api.start();
+            quote.setText("Loading...");
 
-        String welcome_text = "Hello, " + getIntent().getStringExtra("name") + "!";
-        welcome.setText(welcome_text);
-        File internalStorageDir = getFilesDir();
-        File imageFile = new File(internalStorageDir, getIntent().getStringExtra("user") + ".jpg");
-        if (imageFile.exists()) {
-            Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-            userImageView.setImageBitmap(bitmap);
-        } else {
-            userImageView.setImageResource(R.drawable.ic_default_boy);
+            String welcome_text = "Hello, " + getIntent().getStringExtra("name") + "!";
+            welcome.setText(welcome_text);
+            File internalStorageDir = getFilesDir();
+            File imageFile = new File(internalStorageDir, getIntent().getStringExtra("user") + ".jpg");
+            if (imageFile.exists()) {
+                Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+                userImageView.setImageBitmap(bitmap);
+            } else {
+                userImageView.setImageResource(R.drawable.ic_default_boy);
+            }
         }
     }
-}
